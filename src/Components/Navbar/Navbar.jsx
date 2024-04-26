0a046720c0277b0645bdc96a1251b75e81ec6c87
nitinsingha273 <nitinsingha273@gmail.com>
2024-04-27 01:26:16 +0530

    fourth commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar() {
           />
         </NavLink>
 
-        {isMobile && (
+        {isMobile && !isSidebarOpen && (
           <div className="lg:hidden" onClick={toggleSidebar}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
